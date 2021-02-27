5cad5aad73418214ae508c8f1f9d83ce2107597b
Cameron Dempster <camerondem2134@hotmail.com>
2021-02-27 22:27:12 +0000

    Initial application startup implementation

--- RTSPDetector/RTSPDetector/Program.cs
+++ RTSPDetector/RTSPDetector/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace RTSPDetector
+namespace RTSPDetector.WindowsService
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static IServiceProvider _serviceProvider;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RegisterServices();
+            DisposeServices();
+        }
+
+        private static void RegisterServices()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("Environment")}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
+            Log.Information("Service registration is starting...");
+            var services = new ServiceCollection();
+
+            services.AddSingleton<IConfiguration>(config);
+            services.AddSingleton<BackgroundApplication>();
+
+            _serviceProvider = services.BuildServiceProvider(true);
+
+            Log.Information("Service registration complete.");
+        }
+
+        private static void DisposeServices()
+        {
+            Log.Information("Disposing services...");
+
+            if (_serviceProvider == null)
+                return;
+
+            if (_serviceProvider is IDisposable)
+                ((IDisposable)_serviceProvider).Dispose();
+
+            Log.Information("Services have been disposed.");
         }
     }
 }
